15908aae37cd57bdc63b9829e50bd79597672b66
Joe Ulyatt <joewhocodes@gmail.com>
2025-01-11 16:42:12 +0100

    feat: :lipstick: Add icons and welcome text

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,10 +8,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // padding constants
+  final double horizontalPadding = 40;
+  final double verticalPadding = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-    );
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40.0, vertical: 25)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset('lib/icons/menu.png',
+                    height: 45, color: Colors.grey[800]),
+                Icon(
+                  Icons.person,
+                  size: 45,
+                  color: Colors.grey[800],
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: Column(
+                  children: [
+                    Text("Welcome Home,"),
+                    Text("JOE"),
+                  ],
+                )),
+          ],
+        )));
   }
 }
