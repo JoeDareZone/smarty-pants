e4d89a7d400e4f42a89e5dc3fcc518c137b92ccc
Joe Ulyatt <joewhocodes@gmail.com>
2025-01-11 16:56:05 +0100

    feat: :lipstick: Add icon grid

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   // padding constants
   final double horizontalPadding = 40;
-  final double verticalPadding = 100;
+  final double verticalPadding = 25;
+
+  // list of smart devices
+  List mySmartDevices = [
+    ["Smart Light", "lib/icons/light-bulb.png", true],
+    ["Smart AC", "lib/icons/light-bulb.png", false],
+    ["Smart TV", "lib/icons/light-bulb.png", false],
+    ["Smart Fan", "lib/icons/light-bulb.png", false]
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -21,29 +29,51 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40.0, vertical: 25)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset('lib/icons/menu.png',
-                    height: 45, color: Colors.grey[800]),
-                Icon(
-                  Icons.person,
-                  size: 45,
-                  color: Colors.grey[800],
-                )
-              ],
+              padding: EdgeInsets.symmetric(
+                horizontal: horizontalPadding,
+                vertical: verticalPadding,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'lib/icons/menu.png',
+                    height: 45,
+                    color: Colors.grey[800],
+                  ),
+                  Icon(
+                    Icons.person,
+                    size: 45,
+                    color: Colors.grey[800],
+                  )
+                ],
+              ),
             ),
             const SizedBox(height: 20),
             Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
                 child: Column(
-                  children: [
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
                     Text("Welcome Home,"),
-                    Text("JOE"),
+                    Text("JOE", style: TextStyle(fontSize: 40)),
                   ],
                 )),
+            const SizedBox(height: 20),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+                child: Text("Smart Devices")),
+            Expanded(
+                child: GridView.builder(
+                    itemCount: mySmartDevices.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                    itemBuilder: (context, index) {
+                      return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(color: Colors.blue));
+                    }))
           ],
         )));
   }
